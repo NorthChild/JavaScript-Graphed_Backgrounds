b90baa6f3b123c6c79051d1e95cf5d583e66dea2
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2020-10-19 17:05:11 +0200

    Update Graphed_Environment.js

--- Graphed_Environment.js
+++ Graphed_Environment.js
@@ -1,82 +1,72 @@
-////////////////////////////////////////////////////
-// ideally works on 400W x 400H, JavaScript
-////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+// ideally works square canvas (i.e 400x400, 500x500 etc.)
+////////////////////////////////////////////////////////////
+
 // mouse tracker
 var mouse_tracker = function() {
     var x = mouseX - 20;
     var label = mouseX + '/' + mouseY;
-    fill(0, 255, 17);
-    if (mouseX < 315) {
+    fill(0,0,139 );
+    if (mouseX < (width-20) && mouseY < (height-20)) {
         text(label, mouseX + 15, mouseY + 15);
     } else {
         text(label, mouseX - 50, mouseY - 10);
     }
 };
 
-////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
 // spaced lines for the environment
 // x pos
 var graph_x = function() {
     var x = 0;
-    while (x < 400) {
-        line(0, x, 400, x);
+    while (x < width) {
+        line(0, x, width, x);
         x += 10;
     }
 };
 // y pos
 var graph_y = function() {
     var y = 0;
-    while (y < 400) {
-        line(y, 0, y, 400);
+    while (y < height) {
+        line(y, 0, y, height);
         y += 10;
     }
 };
 
-////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
 // ruler for the environment
-//x = 0
+
 var ruler_x_I = function() {
     var x_II = 0;
-    while (x_II < 400) {
+    while (x_II < width) {
         fill(247, 17, 52);
         text(x_II, x_II, 10);
         x_II += 40;
     }
 };
-//x = 400
-var ruler_x_II = function() {
-    var X = 40;
-    while (X < 400) {
-        fill(255, 0, 0);
-        text(X, X, 400);
-        X += 40;
-    }
-};
-// y = 0
+
 var ruler_y_I = function() {
     var y_II = 40;
-    while (y_II < 430) {
+    while (y_II < height) {
         fill(255, 0, 0);
         text(y_II, 0, y_II);
         y_II += 40;
     }
 };
-// y = 379
-var ruler_y_II = function() {
-    var Y = 0;
-    while (Y < 430) {
-        fill(255, 0, 0);
-        text(Y, 379, Y);
-        Y += 40;
-    }
-};
 
-////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+////////////////////////////////////////////////////////////
 // real-time display
-////////////////////////////////////////////////////
-draw = function() {
-    // complementary, who doesnt like the good-old math paper color
-    background(232, 247, 202);
+////////////////////////////////////////////////////////////
+
+
+function draw() {
+  background(232, 247, 202);
 
     // line grid
     graph_x();
@@ -85,13 +75,13 @@ draw = function() {
     // ruler on grid
     // x pos
     ruler_x_I();
-    ruler_x_II();
 
     // y pos
     ruler_y_I();
-    ruler_y_II();
 
     // mouse position tracker
     mouse_tracker();
-};
-////////////////////////////////////////////////////
+}
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
